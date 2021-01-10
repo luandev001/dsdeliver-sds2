5767cc6fe650ded1d2a441acc3a3ef2b2ef9550e
unknown <luan.araujo.santos10@gmail.com>
2021-01-10 19:31:27 -0300

    Search order location

---
+++ front-web/src/Orders/types.tsx
@@ -0,0 +1,16 @@
+import { types } from "util";
+
+export type Product = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imageUri: string;
+ 
+}
+
+export type OrderLocationdata = {
+    address: string;
+    latitude: number;
+    longitude: number;
+}
